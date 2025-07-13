718c3beba29366ca5e57ad037443ce3cae29a411
ramyabasava <150227151+ramyabasava@users.noreply.github.com>
2025-07-13 13:53:47 +0530

    Add files via upload

---
+++ Day 6/jsdemo.js
@@ -0,0 +1,87 @@
+//let x = null;
+//console.log(x)
+
+//let flowers = ["Rose", "Lilly","Tulip"];
+//console.log(flowers)
+
+//let person = {name: "Jhon",age: 30, city: "New York"};
+//console.log(typeof person);
+
+//dialog boxes
+
+// alert() // give the kind of output
+// prompt() // takes the input in box
+// confirm() // confirm dailogbox
+
+//alert("hii ths is my page!");
+//let name1 = prompt("Enter name:");
+//alert(name1);
+// syntax
+
+//expression ?
+
+//let age = prompt("Enter your age:");
+//if (age >= 18) {
+   // alert("you are eligible to vote.");
+//}
+//else {
+    //alert("you are not eligible to vote.");
+//}
+
+//age>=18 ?  alert("you are eligible to vote.")  : alert("you are not eligible to vote.");
+
+// let num = prompt("enter a number: ")
+// if (num>100) {
+//     alert("The number is greater than  100.");
+// } else if (num > 50) {
+//     alert("The number is greater than 50 but less than or equal to 100.");
+// } else if (num > 0) {
+//     alert("The number is greater than 0 but less than or equal to 50.");
+// } else {
+//     alert("the number is less than or equal to 0.");
+// }
+
+// Nested condtional statements 
+// syntax 
+
+// const age = prompt("enter the age");
+// if (age>=17) {
+//     const blood  = prompt("enter the Blood Count");
+//     if(blood  >= 100000) {
+//         const hemo = prompt("enter the Hemoglobin");
+//         if(hemo >65){
+//             const sugar = prompt("enter the Blood sugar");
+//             if(sugar >= 80 && sugar <= 120 ){
+//                 alert("The person is eligibile to donate");
+//             } else {
+//                 alert("The person is Not eligible");
+//             }
+//         } else {
+//             alert("The person is Not eligible");
+//         }
+//     }
+//     else {
+//         alert("The person is Not eligible");
+//     }
+//  } else {
+//     alert("The person is Not eligible");
+// }
+
+// for 
+// while 
+// do while 
+
+// Syntax:
+// for (initialization; condition; increment/decrement) {
+//     // code to be executted 
+// }
+// program to print numbers from 1 to 10 
+// let n = prompt("Enter a number");
+// if (n>0) {
+// for(let i = 1; i <= n; i++){
+//     console.log(i);
+// }
+// }
+// else {
+//     alert("Please enter a positive number")
+// }
